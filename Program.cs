5688b7dd9c153405878f05c39302bbf652c93052
HP-PROBOOK-G10\antco <antonio.kolesnikow@gmail.com>
2025-02-14 21:42:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿// Условие задания:какая из цифр трехзначного числа больше: первая или последняя
+// Чтобы запустить тестовый режим введите 1809
+using LR2_C_;
+
+class Task_1
+{
+    public static string CorrectInput()
+    {
+        bool IsCorrect = false;
+        string Input = "defaultValue";
+        Console.WriteLine("Введите трехзначное число");
+        while (!IsCorrect)
+        {
+            IsCorrect = true;
+            Input = Console.ReadLine();
+
+            if (!int.TryParse(Input, out _))
+            {
+                Console.WriteLine("Некорректный ввод. Повторите попытку");
+                IsCorrect = false;
+            }
+            if (Input.Length != 3)
+            {
+                Console.WriteLine("Некорректный ввод. Повторите попытку");
+                IsCorrect = false;
+            }
+            if (Input == "1809")
+            {
+                return Input;
+            }
+        }
+        return Input;
+    }
+    public static void Main()
+    {
+        string[] tests = { "180", "394", "505", "104",
+                           "217", "325", "438", "509",
+                           "612", "723", "834", "945",
+                           "156", "267", "378", "489",
+                           "590", "601", "712", "823",
+                           "934", "105", "216", "999" };
+        bool testing = false;
+        int TestingIterator = 0;
+        int index = 0;
+        string Input = "defaultValue";
+        while (true)
+        {
+            if (!testing)
+            {
+                Input = CorrectInput();
+            }
+            if (Input == "1809")
+            {
+                testing = true;
+                TestingIterator = 0;
+            }
+            if (testing == true)
+            {
+                Input = tests[TestingIterator++];
+                if (TestingIterator >= tests.Length)
+                {
+                    TestingIterator = 0;
+                    testing = false;
+                }
+            }
+
+            if (Input[0] > Input[2])
+            {
+                index = 0;
+            }
+            if (Input[0] < Input[2])
+            {
+                index = 1;
+            }
+            if (Input[0] == Input[2])
+            {
+                index = 2;
+            }
+            int var = Menu.MenuMethod(Input, index);
+            if (var == 1)
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ menu.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2_C_
+{
+    internal class Menu
+    {
+        public static int MenuMethod(string Input, int index)
+        {
+            string[] menuItems = { "Продолжить", "Закончить" };
+            int selectedIndex = 0;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"Исходное число: {Input}");
+                switch (index)
+                {
+                    case 0:
+                        Console.WriteLine($"первая цифра больше. ({Input[0]} > {Input[2]})");
+                        break;
+                    case 1:
+                        Console.WriteLine($"Последняя цифра больше. ({Input[2]} > {Input[0]})");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Цифры равны. ({Input[0]} = {Input[2]})");
+                        break;
+                }
+
+                for (int i = 0; i < menuItems.Length; ++i)
+                {
+                    if (i == selectedIndex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"> {menuItems[i]}");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"  {menuItems[i]}");
+                    }
+                }
+
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                if (key.Key == ConsoleKey.UpArrow || key.Key == ConsoleKey.W)
+                {
+                    selectedIndex = (selectedIndex == 0) ? menuItems.Length - 1 : selectedIndex - 1;
+                }
+                else if (key.Key == ConsoleKey.DownArrow || key.Key == ConsoleKey.S)
+                {
+                    selectedIndex = (selectedIndex == menuItems.Length - 1) ? 0 : selectedIndex + 1;
+                }
+                else if (key.Key == ConsoleKey.Enter || key.Key == ConsoleKey.Spacebar)
+                {
+                    Console.Clear();
+                    return selectedIndex;
+                }
+            }
+        }
+    }
+}
+
